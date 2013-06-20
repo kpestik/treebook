8b116e85896059ef0ae543c0d73572b614328ce6
kpestik <kpestik@gmail.com>
2013-06-20 15:01:07 -0400

    Added tests for user first name, last name, and profile correctness

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,7 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "a status should not save without a content" do
+		status = Status.new
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+
+	test "a status should not save without a user id" do
+		status = Status.new
+		assert !status.save
+		status.content = "This is a test case"
+		status.user_id = "This is a test user"
+		puts status.errors.inspect
+		assert !status.errors[:user_id].empty?
+	end
 end
